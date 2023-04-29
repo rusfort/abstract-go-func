7935144330a52ae587d4b32d8fee1a4d7057e201
rusfort <rusfort-fax@yandex.ru>
2023-04-29 20:54:09 +0300

    add example 2 in readme

--- src/main.go
+++ src/main.go
@@ -1,33 +1,46 @@
 package main
 
 import (
+	"context"
+
 	"github.com/rusfort/abstract-go-func/functions"
 )
 
 func main() {
 	println("hello")
 
-	// create your new abstract function
-	f1 := functions.NewAbstractFunc(func(params ...interface{}) (interface{}, error) {
-		return SomeFunc(params[0].(int), params[1].(string)) // NOTE: if you feel that someone can make a mess with data types, assert it before return!
+	someStruct := NewSomeStruct("my struct")
+	ctx := context.Background()
+
+	// create your new abstract method with context
+	f2 := functions.NewAbstractMethodWithContext(someStruct, func(self interface{}, ctx context.Context, params ...interface{}) (interface{}, error) {
+		return self.(*SomeStruct).SomeFunc(ctx, params[0].(int), params[1].(string)) // NOTE: if you feel that someone can make a mess with data types, assert it before return!
 	}, 12, "abc") // set default params if needed
 
 	// create a function map for easy key access
 	m := make(functions.FuncMap)
-	m["f1"] = f1
+	m["f2"] = f2
 
 	// access your func by a key
-	f, ok := m["f1"]
+	f, ok := m["f2"]
 	if ok {
 		// run your func with default params
-		_, _ = f.Run()
+		_, _ = f.RunWithContext(ctx)
 		// run your func with custom params
-		_, _ = f.Run(42, "qwerty")
+		_, _ = f.RunWithContext(ctx, 42, "qwerty")
 	}
 }
 
+type SomeStruct struct {
+	name string
+}
+
+func NewSomeStruct(name string) *SomeStruct {
+	return &SomeStruct{name: name}
+}
+
 // here could be anything useful, this is just an example:
-func SomeFunc(a int, b string) (interface{}, error) {
-	println("called f1:", a, b)
+func (s *SomeStruct) SomeFunc(ctx context.Context, a int, b string) (interface{}, error) {
+	println("called f2:", s.name, a, b)
 	return nil, nil
 }
